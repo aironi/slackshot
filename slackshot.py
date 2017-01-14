021e5ec49de1c47dded466a7da777f76cb3ef24a
Iiro Hietala <iiro.hietala@gofore.com>
2017-01-14 15:53:08 +0200

    Added ConfigParser support and default config file

--- slackshot.py
+++ slackshot.py
@@ -6,19 +6,25 @@ import time
 from slacker import Slacker
 
 def main(argv):
-    config = ConfigParser.ConfigParser()
+    config = ConfigParser.ConfigParser({'brightness' : '50', 'contrast' : '50'})
     config.read('slackshot.cfg')
-    apikey = config.get('config', 'api-key')
+    apikey = config.get('slack', 'api-key')
     if apikey is None:
-         print 'Please add api-key to slackshot.cfg under [config] -section'
+         print 'Please add api-key to slackshot.cfg under [slack] -section'
          sys.exit(2)
 
-    start_capture(apikey)
+    brightness = config.getint('camera', 'brightness')
+    contrast = config.getint('camera', 'contrast')
+    camera_config = { 'brightness' : brightness, 'contrast' : contrast }
+    start_capture(apikey, camera_config)
 
 
-def start_capture(apikey):
+def start_capture(apikey, camera_config):
     # add loop
     with picamera.PiCamera() as camera:
+        camera.brightness = camera_config['brightness']
+        camera.contrast = camera_config['contrast']
+
         camera.start_preview()
 
         time.sleep(3)
